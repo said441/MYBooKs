97b50b896cd86abfc7bba9bc12ac0fa2026e6211
said441 <saidsouda89@gmail.com>
2019-12-05 01:23:53 +0100

    trvaille de deux jour

---
+++ app/src/main/java/com/example/mybooks/Interface/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.example.mybooks.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/example/mybooks/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.mybooks.Prevalent;
+
+import com.example.mybooks.Model.User;
+
+public class Prevalent {
+    public static User currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
